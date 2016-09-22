b22868c3654cce8185eacc61fe4fb89ff40c0dd3
rebekahheacock <rebekah.heacock@gmail.com>
2016-09-22 14:34:37 -0400

    Add create for ingredients
    Update routes
    Add controller method

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -5,6 +5,10 @@ class IngredientsController < ApplicationController
     @ingredient = Ingredient.find(params[:id])
   end
 
+  def ingredient_params
+    params.require(:ingredient).permit(:name, :std_measure, :color)
+  end
+
   def index
     @ingredients = Ingredient.all
     render json: @ingredients
@@ -14,6 +18,15 @@ class IngredientsController < ApplicationController
     render json: @ingredient
   end
 
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
 
-  private :set_ingredient
+    if @ingredient.save
+      render json: @ingredient, status: :created
+    else
+      render json: @ingredient.errors, status: :unprocessable_entity
+    end
+  end
+
+  private :set_ingredient, :ingredient_params
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
 
   get '/ingredients' => 'ingredients#index'
   get '/ingredients/:id' => 'ingredients#show'
+  post '/ingredients/' => 'ingredients#create'
 end
