c20dcf3d6c5e4cc8cc647059b06461424ac60f31
rebekahheacock <rebekah.heacock@gmail.com>
2016-09-25 15:07:52 -0400

    Add examples
    Add example recipes and ingredients

--- db/examples.rb
+++ db/examples.rb
@@ -16,3 +16,46 @@
 #                password: 'abc123',
 #                password_confirmation: nil)
 # end
+
+Recipe.create([
+                { name: 'Sugar Cookies', description: 'Mmmm cookies.' },
+                { name: 'Grilled Cheese', description: 'So good.' }
+              ])
+
+Ingredient.create([
+                    {
+                      name: 'flour',
+                      std_measure: 'cup',
+                      color: 'white',
+                      toxicity: false,
+                      recipe_id: 1
+                    },
+                    {
+                      name: 'sugar',
+                      std_measure: 'cup',
+                      color: 'white',
+                      toxicity: false,
+                      recipe_id: 1
+                    },
+                    {
+                      name: 'butter',
+                      std_measure: 'stick',
+                      color: 'yellow',
+                      toxicity: false,
+                      recipe_id: 1
+                    },
+                    {
+                      name: 'bread',
+                      std_measure: 'slice',
+                      color: 'tan',
+                      toxicity: false,
+                      recipe_id: 2
+                    },
+                    {
+                      name: 'cheese',
+                      std_measure: 'oz',
+                      color: 'white',
+                      toxicity: false,
+                      recipe_id: 2
+                    }
+                  ])
